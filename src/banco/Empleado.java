b7f5d86f88b9ad1c0df13ba9c00b68bd33a5504e
profesorjacinto15 <profesor.jacinto15@gmail.com>
2019-04-09 00:16:48 +0100

    Commit 3 - Clase Empleado

---
+++ src/banco/Empleado.java
@@ -0,0 +1,31 @@
+package banco;
+
+public class Empleado {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
